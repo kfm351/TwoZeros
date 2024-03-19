430d688360b4a4d5b6b05f2a50193714a472c7d2
kfm351 <elina.gav@yandex.ru>
2024-03-20 01:54:53 +0300

    Добавьте файлы проекта.

---
+++ TwoZeros/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(recursion(1,2));
+
+int recursion(int a, int b)
+{
+    if (a > b + 1)
+    {
+        return 0;
+    }
+    if (a == 0 || b == 0)
+    {
+        return 1;
+    }
+    return recursion(a, b - 1) + recursion(a - 1, b - 1);
+}
